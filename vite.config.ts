7da8306446c4e8362311acfcb84c35fdc06a90eb
yiselorena0808 <yiselorenasalzar@gmail.com>
2025-11-27 17:28:34 -0500

    password

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-    port: 5173,
+ server: {
     proxy: {
       "/capture": "http://10.3.43.191:5000"
-    }
-  },
-  base: '/',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
     }
   }
 })
